469af892a1d05172eef3ec2cc6ecce34dd0ca730
Serhii Dymenko <serge.sergeevich@gmail.com>
2023-10-29 21:40:44 -0400

    Refactoring Message Bubble component

--- lib/components/message_bubble.dart
+++ lib/components/message_bubble.dart
@@ -1,35 +1,48 @@
 import 'package:flutter/material.dart';
 
 class MessageBubble extends StatelessWidget {
-  MessageBubble({this.sender, this.text});
+  MessageBubble({required this.sender, required this.text, required this.isMe});
 
-  final String? sender;
-  final String? text;
+  late final String sender;
+  late final String text;
+  late final bool isMe;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.all(10.0),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
+        crossAxisAlignment:
+            isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+        children: <Widget>[
           Text(
-            '$sender',
+            sender,
             style: TextStyle(
-              fontSize: 12,
+              fontSize: 12.0,
               color: Colors.black54,
             ),
           ),
           Material(
-            borderRadius: BorderRadius.circular(30),
+            borderRadius: isMe
+                ? BorderRadius.only(
+                    topLeft: Radius.circular(30.0),
+                    bottomLeft: Radius.circular(30.0),
+                    bottomRight: Radius.circular(30.0))
+                : BorderRadius.only(
+                    bottomLeft: Radius.circular(30.0),
+                    bottomRight: Radius.circular(30.0),
+                    topRight: Radius.circular(30.0),
+                  ),
             elevation: 5.0,
-            color: Colors.lightBlueAccent,
+            color: isMe ? Colors.lightBlueAccent : Colors.white,
             child: Padding(
-              padding:
-                  const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
               child: Text(
-                '$text',
-                style: TextStyle(color: Colors.white, fontSize: 15),
+                text,
+                style: TextStyle(
+                  color: isMe ? Colors.white : Colors.black54,
+                  fontSize: 15.0,
+                ),
               ),
             ),
           ),
